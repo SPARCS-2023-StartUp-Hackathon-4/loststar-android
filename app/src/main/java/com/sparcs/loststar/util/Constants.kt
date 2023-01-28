c25334d1224952341f32fc6dc01176e3358c6199
koownij <koownij@kakao.com>
2023-01-28 15:53:53 +0900

    feat: base url 변경

--- app/src/main/java/com/sparcs/loststar/util/Constants.kt
+++ app/src/main/java/com/sparcs/loststar/util/Constants.kt
@@ -3,5 +3,5 @@ package com.sparcs.loststar.util
 object Constants {
     const val KAKAO_NATIVE_KEY = "70f66fdbf4ae1764954ef7bf16ddae9d"
     const val RETROFIT_TAG = "Retrofit"
-    const val BASE_URL = "http://www.plub.co.kr"
+    const val BASE_URL = "http://15.165.222.2"
 }
