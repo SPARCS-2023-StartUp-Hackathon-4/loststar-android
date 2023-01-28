b6dd35f0b2cc498cdb1b2e8e4532ee0664e44f41
koownij <koownij@kakao.com>
2023-01-28 19:56:32 +0900

    feat: 신고 페이지 1차

--- app/src/main/java/com/sparcs/loststar/ui/report/Report1Fragment.kt
+++ app/src/main/java/com/sparcs/loststar/ui/report/Report1Fragment.kt
@@ -5,14 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.sparcs.loststar.R
 import com.sparcs.loststar.databinding.FragmentReport1Binding
+import com.sparcs.loststar.util.afterTextChanged
 
 class Report1Fragment : Fragment() {
 
     private val binding: FragmentReport1Binding by lazy {
         FragmentReport1Binding.inflate(layoutInflater)
     }
+
+    private val editTextList: List<EditText> by lazy {
+        listOf(
+            binding.etName,
+            binding.etCategory,
+            binding.etRegion,
+            binding.etRegionDetail,
+            binding.etDate,
+            binding.etTime
+        )
+    }
+
+    private val textViewList: List<TextView> by lazy {
+        listOf(
+            binding.tvName,
+            binding.tvCategory,
+            binding.tvRegion,
+            binding.tvRegionDetail,
+            binding.tvDate,
+            binding.tvTime
+        )
+    }
+
+    private fun initEditTextLayoutSetting() {
+        for(idx in editTextList.indices) {
+            editTextList[idx].afterTextChanged { editable ->
+                val text = editable.toString()
+                if(text.isEmpty()) {
+                    textViewList[idx].setTextColor(ContextCompat.getColor(requireContext(), R.color.color_7e7e7e))
+                    editTextList[idx].setBackgroundResource(R.drawable.bg_rectangle_empty_7e7e7e_radius_14)
+                } else {
+                    textViewList[idx].setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+                    editTextList[idx].setBackgroundResource(R.drawable.bg_rectangle_empty_white_radius_14)
+                }
+            }
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +65,8 @@ class Report1Fragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initEditTextLayoutSetting()
 
         (parentFragment as ReportFragment).nextButton.isEnabled = false
     }
--- app/src/main/java/com/sparcs/loststar/util/Extension.kt
+++ app/src/main/java/com/sparcs/loststar/util/Extension.kt
@@ -1,4 +1,24 @@
 package com.sparcs.loststar.util
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
 fun String?.isJsonObject(): Boolean = this?.startsWith("{") == true && this.endsWith("}")
 fun String?.isJsonArray(): Boolean = this?.startsWith("[") == true && this.endsWith("]")
+
+fun EditText.afterTextChanged(method: (editable: Editable?) -> Unit) {
+    addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+            method(p0)
+        }
+    })
+}
